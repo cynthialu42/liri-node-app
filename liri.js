6a396e85794bc025ae9a10ac6de8dc0b325de0e6
cynthialu42 <clu2334@gmail.com>
2018-04-15 18:49:19 -0500

    finished twitter part

--- liri.js
+++ liri.js
@@ -10,10 +10,28 @@ var client = new twitter(keys.twitter);
 
 // commands
 var command = process.argv[2];
+//console.log("Command is: " +command);
+if (command === "my-tweets"){
+    var params = {screen_name: 'sadmayo'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            var numOfTweets = 0;
 
-var params = {screen_name: 'sadmayo'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets[0].text);
-    }
-  });
+            // If less than 20 tweets, show what's available
+            if (tweets.length < 20){
+                numOfTweets = tweets.length;
+            }
+            else{
+                numOfTweets = 20;
+            }
+            for (let i = 0; i < numOfTweets; i++){
+                console.log("============== " + tweets[i].created_at + " ============== \n")
+                console.log(tweets[0].text + " ~" + params.screen_name + "\n");
+            } 
+            console.log("============================================================")
+        }
+    });
+}
+else{
+    console.log("no");
+}
